555c0deea2122cb740e21251964d1bda1f580509
strozj <strozykjacob@gmail.com>
2022-04-19 18:05:47 -0700

    Refactored hash verification page and implemented verification funcitonality.

--- block-health/src/App.js
+++ block-health/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavBar } from './NavigationBar.js';
 import { Route, Routes } from 'react-router-dom';
 import { WorkerPage } from './Worker.js';
-import { HashVerificationPage } from './HashVerificationPage.js';
+import { HashVerificationForm } from './HashVerificationForm.js';
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/home"></Route>
         <Route path="/workers" element={<WorkerPage/>}></Route>
-        <Route path="/hashverify" element={<HashVerificationPage/>}></Route>
+        <Route path="/hashverify" element={<HashVerificationForm/>}></Route>
       </Routes>
     </div>
   );
