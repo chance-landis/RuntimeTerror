927f6fa6c2d5ef30f4b42c468e80544fab18f177
strozj <strozykjacob@gmail.com>
2022-05-04 15:50:17 -0700

    Fixed variables

--- block-health/src/util/interact.js
+++ block-health/src/util/interact.js
@@ -1,8 +1,8 @@
 import { pinJSONToIPFS } from "./pinata.js";
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const contractABI = require("../contract-abi.json");
-const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
-const PRIVATE_KEY = "8552405dd66448062be03ce704667942323c58a64348ac8ad1d835bca34c0fae"
+const fromWalletID = '0xb884BeeeED09B787d12253058c55cdA07DBB6FEa'
+const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
@@ -31,10 +31,10 @@ export async function mintNFT(targetWalletID) {
 
   const transactionParameters = {
     to: targetWalletID, // Required except during contract publications.
-    from: '0xb884BeeeED09B787d12253058c55cdA07DBB6FEa', // must match user's active address.
+    from: fromWalletID, // must match user's active address.
     gas: 600000,
     data: contract.methods
-      .mintNFT('0xb884BeeeED09B787d12253058c55cdA07DBB6FEa', tokenURI)
+      .mintNFT(fromWalletID, tokenURI)
       .encodeABI(),
   };
 
